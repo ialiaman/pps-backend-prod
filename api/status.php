cd236da20c5a91a23768bbbe469e310d92ffda8a
Sheheryarktk975 <97036156+Sheheryarktk975@users.noreply.github.com>
2022-01-18 19:39:52 +0500

    api

---
+++ api/status.php
@@ -0,0 +1,22 @@
+<?php
+
+include('config.php');
+header("Content-Type:application/json");
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
+$data = json_decode(file_get_contents("php://input"), true);
+$ip = $_POST['ip'];
+echo json_encode($ip);
+
+?>
+
+
+
+
+
+
+
