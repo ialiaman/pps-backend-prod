aa3c3dc23663dbf8150d2118f1b937e458a39434
ialiaman <iamanhunzai@gmail.com>
2022-01-19 21:09:04 +0500

    v1

--- api/status.php
+++ api/status.php
@@ -11,44 +11,42 @@ include('config.php');
 $data = json_decode(file_get_contents("php://input"), true);
 $type = @$data['type'];
 
-echo json_encode($type);
+if ($type == 1)
+{
+    global $con;
+    $ip = $data['ip'];
+    $page= $data['page'];
+    if ($ip != null)
+    {
+        $getCurrentCount = $con -> query("SELECT count FROM visit_count WHERE page_name = '$page' ");
 
-// if ($type == 1)
-// {
-//     global $con;
-//     $ip = $_POST['ip'];
-//     $page= $_POST['page'];
-//     if ($ip != null)
-//     {
-//         $getCurrentCount = $con -> query("SELECT count FROM visit_count WHERE page_name = '$page' ");
+        $count = mysqli_fetch_assoc($getCurrentCount);
+        $newcount = intVal($count['count'])+1;
 
-//         $count = mysqli_fetch_assoc($getCurrentCount);
-//         $newcount = intVal($count['count'])+1;
-
-//         $querycount = "UPDATE visit_count set count = '$newcount' WHERE page_name = '$page'";
-//         $querycount_run = mysqli_query($con,$querycount);
-//         if($querycount_run)
-//         {
-//             $data = array("response" => 1);
-//             echo json_encode($data);
-//         }
-//         else
-//         {
-//             $data = array("response" => 0);
-//             echo json_encode($data);
-//         }
-//     }
-//     else
-//     {
-//         $data = array("No IP Address Recieved");
-//         echo json_encode($data);
-//     } 
-// }
-// else
-// {
-//     $data = array("Type not Same");
-//     echo json_encode($data);
-// }
+        $querycount = "UPDATE visit_count set count = '$newcount' WHERE page_name = '$page'";
+        $querycount_run = mysqli_query($con,$querycount);
+        if($querycount_run)
+        {
+            $data = array("response" => 1);
+            echo json_encode($data);
+        }
+        else
+        {
+            $data = array("response" => 0);
+            echo json_encode($data);
+        }
+    }
+    else
+    {
+        $data = array("No IP Address Recieved");
+        echo json_encode($data);
+    } 
+}
+else
+{
+    $data = array("Type not Same");
+    echo json_encode($data);
+}
 
 // if ($type == 2)
 // {
