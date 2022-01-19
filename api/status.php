ea447821151f1d2a6515257cfd4887f30c6ef112
ialiaman <iamanhunzai@gmail.com>
2022-01-19 21:04:37 +0500

    v1

--- api/status.php
+++ api/status.php
@@ -1,12 +1,12 @@
 <?php
-
-include('config.php');
 header("Content-Type:application/json");
 header('Access-Control-Allow-Origin: *'); 
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+include('config.php');
+
 
 $data = json_decode(file_get_contents("php://input"), true);
 $type = @$_POST['type'];
