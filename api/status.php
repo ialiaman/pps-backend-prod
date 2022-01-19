4a8f4660ef27a4c81a218f76cab99e2aa4826cd0
Sheheryarktk975 <97036156+Sheheryarktk975@users.noreply.github.com>
2022-01-19 19:15:07 +0500

    Api Updated

--- api/status.php
+++ api/status.php
@@ -8,7 +8,7 @@ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
-$data = json_decode(file_get_contents("php://input"), true);
+// $data = json_decode(file_get_contents("php://input"), true);
 $type = @$_POST['type'];
 if ($type == 1)
 {
@@ -18,12 +18,30 @@ if ($type == 1)
     if ($ip != null)
     {
         $getCurrentCount = $con -> query("SELECT count FROM visit_count WHERE page_name = '$page' ");
-        echo json_encode($page);
 
+        $count = mysqli_fetch_assoc($getCurrentCount);
+        $newcount = intVal($count['count'])+1;
+
+        $querycount = "UPDATE visit_count set count = '$newcount' WHERE page_name = '$page'";
+        $querycount_run = mysqli_query($con,$querycount);
+        if($querycount_run)
+        {
+            echo json_encode('1');
+        }
+        else
+        {
+            echo json_encode('0');
+        }
     }
-    
+    else
+    {
+        echo json_encode('No IP Address Recieved');
+    } 
 }
-
+else
+{
+    echo json_encode('Type not Same');
+}
 ?>
 
 
