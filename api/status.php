ff723db6eab2df4412e9cb81304dd0cefff66ec4
Sheheryarktk975 <97036156+Sheheryarktk975@users.noreply.github.com>
2022-01-19 20:04:07 +0500

    APi Updated

--- api/status.php
+++ api/status.php
@@ -8,7 +8,7 @@ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
-// $data = json_decode(file_get_contents("php://input"), true);
+$data = json_decode(file_get_contents("php://input"), true);
 $type = @$_POST['type'];
 if ($type == 1)
 {
@@ -26,11 +26,13 @@ if ($type == 1)
         $querycount_run = mysqli_query($con,$querycount);
         if($querycount_run)
         {
-            echo json_encode('1');
+            $data = array("response" => 1);
+            echo json_encode($data);
         }
         else
         {
-            echo json_encode('0');
+            $data = array("response" => 0);
+            echo json_encode($data);
         }
     }
     else
@@ -42,6 +44,40 @@ else
 {
     echo json_encode('Type not Same');
 }
+
+// if ($type == 2)
+// {
+//     global $con;
+//     $ip = $_POST['ip'];
+//     $page= $_POST['page'];
+//     if ($ip != null)
+//     {
+//         $getCurrentCount = $con -> query("SELECT count FROM visit_count WHERE page_name = '$page' ");
+
+//         $count = mysqli_fetch_assoc($getCurrentCount);
+//         $newcount = intVal($count['count'])+1;
+
+//         $querycount = "UPDATE visit_count set count = '$newcount' WHERE page_name = '$page'";
+//         $querycount_run = mysqli_query($con,$querycount);
+//         if($querycount_run)
+//         {
+//             echo json_encode('1');
+//         }
+//         else
+//         {
+//             echo json_encode('0');
+//         }
+//     }
+//     else
+//     {
+//         echo json_encode('No IP Address Recieved');
+//     } 
+// }
+// else
+// {
+//     echo json_encode('Type not Same');
+// }
+
 ?>
 
 
